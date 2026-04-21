b042d949171ec8b2562c33df0af2785ff20818f9
Adam Foflonker <87928802+DnA-IntRicate@users.noreply.github.com>
2026-04-21 09:26:58 +0200

    Problem 4: Median of Two Sorted Arrays (#1)
    * C++ solution
    
    * Python solution

---
+++ 4. Median of Two Sorted Arrays/Solution.py
@@ -0,0 +1,11 @@
+from typing import List
+
+from numpy import median
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        return median(sorted(nums1 + nums2))
+
+    
+s = Solution()
+print(s.findMedianSortedArrays([1, 2], [3, 4]))
