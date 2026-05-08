aa018fe7ba897f94f5fe61a07227676c72f860fd
Adam Foflonker <87928802+DnA-IntRicate@users.noreply.github.com>
2026-05-08 18:20:25 +0200

    C++ solution

---
+++ 509. Fibonacci Number/solution.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+
+class Solution
+{
+public:
+    int fib(int n)
+    {
+        std::vector<int> memo(n + 1, -1);
+        return fib(n, memo);
+    }
+
+    int fib(int n, std::vector<int>& memo)
+    {
+        if (n <= 1)
+            return n;
+
+        if (memo[n] != -1)
+            return memo[n];
+
+        memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+        return memo[n];
+    }
+};
