bd740f9e6e47562abec6af0b08188e5d283c3a05
Adam Foflonker <87928802+DnA-IntRicate@users.noreply.github.com>
2026-05-12 13:40:45 +0200

    Optimized C solution

--- 12. Integer to Roman/solution.c
+++ 12. Integer to Roman/solution.c
@@ -1,22 +1,24 @@
-char to_numeral(int num)
-{
-    switch (num)
-    {
-        case 1: return "I";
-        case 5: return "V";
-        case 10: return "X";
-        case 50: return "L";
-        case 100: return "C";
-        case 500: return "D";
-        case 1000: return "M";
-    }
-
-    return "";
-}
+#include <string.h>
+#include <stdlib.h>
 
 
 char* intToRoman(int num)
 {
-    int len = 0;
+    // Roman numerals have a tiny fixed range:
+    // 1 <= num <= 3999
 
+    // Digit lookup tables
+    const const char* THOUSANDS[] = { "", "M", "MM", "MMM" };
+    const const char* HUNDREDS[]  = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+    const const char* TENS[]      = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+    const const char* ONES[]      = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+    // Max length is 15 chars
+    char* res = (char*)malloc(16 * sizeof(char));
+    (void)strcpy(res, THOUSANDS[num / 1000]);
+    (void)strcat(res, HUNDREDS[(num % 1000) / 100]);
+    (void)strcat(res, TENS[(num % 100) / 10]);
+    (void)strcat(res, ONES[num % 10]);
+
+    return res;
 }
