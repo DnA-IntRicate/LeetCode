b828a97a2b8143342cce62f4c6cc8b7fc6cd9cb9
Adam Foflonker <87928802+DnA-IntRicate@users.noreply.github.com>
2026-05-20 11:04:55 +0200

    Assembly solution

--- 69. Sqrt(x)/asm_solution.c
+++ 69. Sqrt(x)/asm_solution.c
@@ -1,21 +1,20 @@
 __attribute__((naked)) int mySqrt(int x)
 {
-    // Parameter x is in edi
+    // Param x is in edi
     // Return register is eax
 
     __asm__(
-        ".intel_syntax noprefix; \
-        push rbp; \
-        mov rbp, rsp; \
-        sub rsp, 16; \
-        \
-        \
-        xor eax, eax; \
-        mov eax, edi; \
-        \
-        mov rsp, rbp; \
-        pop rbp; \
-        ret; \
-        .att_syntax; \
-    ");
+        ".intel_syntax noprefix\n"
+
+        // xmm0 = (double)x
+        "cvtsi2sd xmm0, edi\n"
+
+        // xmm0 = sqrt(xmm0)
+        "sqrtsd xmm0, xmm0\n"
+
+        // convert back to int (truncate toward 0)
+        "cvttsd2si eax, xmm0\n"
+
+        "ret\n"
+        ".att_syntax\n");
 }
